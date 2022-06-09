c994eb6011542c47c8b4ad087b8046484c2189b7
Hamza Zubair <hamzamohdzubair@github.com>
2022-06-09 20:12:07 +0530

    vid code. initial commit

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,31 @@ struct Article {
 fn get_articles(url: &str) -> Result<Articles, Box<dyn std::error::Error>> {
     let response = ureq::get(url).call()?.into_string()?;
     let articles: Articles = serde_json::from_str(&response)?;
-    dbg!(articles);
-    todo!()
+    Ok(articles)
+    // dbg!(articles);
+    // todo!()
     // Ok(response)
 }
 
-fn main() {
+// fn main() {
+//     let url = "https://ewsapi.org/v2/top-headlines?country=us&apiKey=a0998ee4103d47efbe9b1853f1006fd6";
+//     let articles = get_articles(url).unwrap();
+//     dbg!(articles);
+
+// }
+
+fn render_articles(articles: &Articles){
+    for i in &articles.articles {
+        colour::dark_green_ln!("> {}", i.title);
+        colour::yellow_ln!("({})", i.url);
+        println!();
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a0998ee4103d47efbe9b1853f1006fd6";
-    let articles = get_articles(url);
+    let articles = get_articles(url)?;
+    render_articles(&articles);
+    // dbg!(articles);
+    Ok(())
 }
